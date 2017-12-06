8f0bcba3d5dc0290494d23811c17f9c0456d4cc2
rprioul <remy.prioul@gmail.com>
2017-12-06 16:20:18 +0100

    updated messages

--- src/index.js
+++ src/index.js
@@ -29,17 +29,13 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
 	console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`);
 });
 
-rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
- rtm.sendMessage("Beep boop ! Coming online, call me by pressing", channel);
-});
-
 rtm.on(RTM_EVENTS.MESSAGE, (message) => {
 	let lMess = message.text.toLowerCase();
   if (lMess === "!bitcoin") {
 	  return new Promise((resolve) => {
 			request(reqOpts).then((body) => {
 				body = JSON.parse(body);
-				const text = 'On ' + body.time.updated + ' ' +', the Bitcoin value is ' + body.bpi.EUR.rate + ' €.';
+				const text = 'On ' + body.time.updated + ' ' +', the Bitcoin value is `' + body.bpi.EUR.rate + ' €` or `' + body.bpi.USD.rate + ' $`.';
 				rtm.sendMessage(text, channel);
 			}) // request.then
 			.catch((err) => {
