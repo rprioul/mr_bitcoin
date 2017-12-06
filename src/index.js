d8e9cb730b4de48e5af47518e6d0c53a243bb89c
rprioul <remy.prioul@gmail.com>
2017-12-06 16:40:32 +0100

    cleaning up code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const {
   RtmClient,
-  WebClient,
   CLIENT_EVENTS,
   RTM_EVENTS,
 } = require('@slack/client');
@@ -11,38 +10,31 @@ const reqOpts = {
   url: 'https://api.coindesk.com/v1/bpi/currentprice.json',
 };
 
-
 const token = process.env.BOT_API_KEY;
-//const token = 'xoxb-282764704598-gvoGR5lfXVraDTKszqnwidig';
 const rtm = new RtmClient(token);
 
 let channel;
 
-const isMessage = (event) => event.type === 'message' && event.text;
-
-const isMessageFromUser = (event, userId) => event.user === userId;
-
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
 	for (const c of rtmStartData.channels) {
 		if (c.is_member && c.name ==='général') { channel = c.id }
-	}
+	} // for
 	console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`);
-});
+}); // rtm.on(rtmStartData)
 
 rtm.on(RTM_EVENTS.MESSAGE, (message) => {
-	let lMess = message.text.toLowerCase();
-  if (lMess === "!bitcoin") {
+  if (message.text.toLowerCase() === "!bitcoin") {
 	  return new Promise((resolve) => {
 			request(reqOpts).then((body) => {
 				body = JSON.parse(body);
 				const text = 'On ' + body.time.updated + ' ' +', the Bitcoin value is `' + body.bpi.EUR.rate + ' €` or `' + body.bpi.USD.rate + ' $`.';
-				rtm.sendMessage(text, channel);
+				return(rtm.sendMessage(text, channel));
 			}) // request.then
 			.catch((err) => {
 				return console.error(`request for ${ reqOpts.url } failed: ${ err }`);
 			}); // request.catch
 		}); // new Promise
-	}
-});
+	} // if
+}); // rtm.on(message)
 
 rtm.start();
